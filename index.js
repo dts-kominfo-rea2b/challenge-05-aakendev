7bcc7bff4f0f0d10b9c23f50e0cf059e661b7b1c
aakendev <aakendev@gmail.com>
2022-06-06 15:18:54 +0000

    Challenge 05 DTS 2022 - Callback

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+
+// DEBUG Mode
+// const nameAsc = names.sort().map((item) => {return `${names.indexOf(item) + 1}. ${item}`});
+// const nameDesc = names.reverse().map((item) => {return `${names.indexOf(item) + 1}. ${item}`});
+// console.log(nameAsc);
+// console.log(nameDesc);
+// console.log(names.sort());
+// console.log(names.reverse());
+
+const sorter = (names, sortMethod) => {
+  const sorting = sortMethod(names);
+  const newData = names.map((item) => {return `${names.indexOf(item) + 1}. ${item}`});
+  return newData;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => names.sort();
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) => names.reverse();
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
